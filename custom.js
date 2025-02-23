7544d24388850ee8f469f27a0dc6fc458251012c
RupmNath <128634103+RupmNath@users.noreply.github.com>
2025-02-23 10:00:14 +0530

    added untracked files before merging

---
+++ custom.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTop");
+
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
